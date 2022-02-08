e93d8c67fdf0ac3d0339f54be1f9d0a49bf1bad2
Yosra Emad <yosraemad14@gmail.com>
2022-02-08 17:50:56 +0200

    TODO app UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO List</h1>
+      <form className="App-form">
+        <input type="text" />
+        <button type="submit">+</button>
+      </form>
+      <div className="Todos">
+        <TodoItem text="Watch TV" />
+        <TodoItem text="Play the Piano" />
+        <TodoItem text="Feed the cats" />
+      </div>
     </div>
   );
 }
